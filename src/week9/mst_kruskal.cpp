26091497a60f15f8969aff694fc21d255fc6d516
peru0 <65155257+peru0@users.noreply.github.com>
2021-05-04 16:17:40 +0900

    Add kruskal

--- src/week9/mst_kruskal.cpp
+++ src/week9/mst_kruskal.cpp
@@ -1,61 +1,203 @@
+#include <cstddef>
 #include <vector>
 #include <exception>
+#include <memory>
+#include <string>
+#include <algorithm>
+#include <iostream>
+//#include <format>
 
 class UnionFind {
-    std::vector<int32_t> m_parent;
-
+    std::vector<size_t> m_parents;
+    std::vector<size_t> m_weights;
 public:
-    explicit UnionFind(int32_t elements);
+    // C++에 아직 안 익숙해서 아직 언제 copy-init가 일어나는 지 잘 모르니까...
+    explicit UnionFind(size_t elements) : m_parents(), m_weights(elements, 1) {
+        m_parents.reserve(elements);
+        for (int i = 0; i < elements; i++) {
+            m_parents.push_back(i);
+        }
+    }
 
-    void make_union(int32_t x_index, int32_t y_index);
-    int32_t find_root(int32_t index);
-    bool is_in_same_set(int32_t x_index, int32_t y_index);
+    void make_union(size_t x_index, size_t y_index) {
+        auto x_root = find_root(x_index);
+        auto y_root = find_root(y_index);
+
+        auto x_weight = m_weights[x_root];
+        auto y_weight = m_weights[y_root];
+
+        if (x_weight < y_weight) {
+            // Let x be the child of y
+            m_parents[x_root] = y_root;
+            m_weights[y_root] += m_weights[x_root];
+        } else {
+            // Let y be the child of x
+            m_parents[y_root] = x_root;
+            m_weights[x_root] += m_weights[y_root];
+        }
+    }
+
+    size_t find_root(size_t index) {
+        size_t id = index;
+        while (id != m_parents[id]) {
+            m_parents[id] = m_parents[m_parents[id]];
+            id = m_parents[id];
+        }
+
+        return id;
+    }
+
+    bool is_in_same_set(size_t x_index, size_t y_index) {
+        auto x_root = find_root(x_index);
+        auto y_root = find_root(y_index);
+
+        return x_root == y_root;
+    }
+};
+
+struct Edge {
+    size_t source;
+    size_t destination;
+    uint32_t weight;
 };
 
 class Graph {
-    std::vector<int32_t> m_edges;
+    // kruskal 구현할 때 가장 까다롭다고 느낀 게
+    // 일단 그래프 내의 모든 edge 목록을 따로 가지고 있어야 오름차순으로 정렬할 때 logN이나 NlogN 안에 가능한데
+    // (아니면 모든 정점을 순회하고 나서야 edges를 정렬할 수 있으니까...)
+    //
+    // 이러면 vertex를 그래프에서 삭제하는 연산이 필요할 때
+    // (예를 들어 게임 내에서 문이 열리고 닫힐 때마다 NPC가 지나가지 못하는 경로를 찾지 못 하도록 실내/실외를 연결하는 간선이 변한다던가)
+    // edges를 전부 조회하고 배열에서 삭제하는 연산을 해야 한단 말이지...
+    //
+    // 아무래도 다익스트라를 구현할 때 고려해봐야겠다.
+    std::vector<std::string> m_vertices;
+    std::vector<Edge> m_edges;
+
 public:
+    explicit Graph() : m_vertices(), m_edges() {
+    }
 
+    void insert_node(const std::string& name) {
+        // 'take ownership of name'을 쉽게 할 방법이 없고 그냥 복사...
+        m_vertices.push_back(name);
+    }
 
+    template<class StringIteratorType>
+    void insert_node(StringIteratorType begin, StringIteratorType end) {
+        for (; begin != end; begin++) {
+            insert_node(*begin);
+        }
+    }
+
+    void insert_edge(const Edge& edge) {
+        if (edge.source < 0 || edge.source > m_edges.size())
+            // https://docs.microsoft.com/en-us/cpp/overview/visual-cpp-language-conformance?view=msvc-160
+            // P0645R10 <format> Text Formatting 	No
+//            throw std::exception(std::format("index '{}' is invalid"));
+            throw std::exception("provided source vertex index is invalid");
+
+        if (edge.source < 0 || edge.source > m_edges.size())
+            throw std::exception("provided destination vertex index is invalid");
+
+        m_edges.push_back(edge);
+    }
+
+    template<class IteratorType>
+    void insert_edge(IteratorType begin, IteratorType end) {
+        for (; begin != end; begin++) {
+            insert_edge(*begin);
+        }
+    }
+
+    void print_kruskal_mst() {
+        UnionFind sets{m_vertices.size()};
+
+//        std::vector<Edge> path{};
+//        path.reserve(m_vertices.size());
+
+        std::sort(m_edges.begin(), m_edges.end(), [] (const Edge& lhs, const Edge& rhs) {
+            return lhs.weight < rhs.weight;
+        });
+
+        size_t total_weight = 0;
+        size_t num_connected = 0;
+
+        for (const Edge& edge : m_edges) {
+            if (sets.is_in_same_set(edge.source, edge.destination)) {
+                std::cout
+                        << "connecting "
+                        << m_vertices[edge.source]
+                        << " <-" << edge.weight  << "-> "
+                        << m_vertices[edge.destination]
+                        << " would make a cycle, skipping.."
+                        << std::endl;
+                continue;
+            }
+
+//            path.push_back(edge.source);
+
+            std::cout
+                << m_vertices[edge.source]
+                << " <-" << edge.weight  << "-> "
+                << m_vertices[edge.destination]
+                << std::endl;
+            sets.make_union(edge.source, edge.destination);
+
+            total_weight += edge.weight;
+            num_connected += 1;
+
+            if (num_connected + 1 >= m_vertices.size()) {
+                std::cout << "|S| + 1 == |V|" << std::endl;
+                break;
+            }
+        }
+
+        std::cout << "connected edges: " << num_connected << std::endl;
+        std::cout << "total weight: " << total_weight << std::endl;
+    }
 };
 
-UnionFind::UnionFind(int32_t elements) : m_parent() {
-    m_parent.reserve(elements);
-    for (auto i = 0; i < elements; ++i) {
-        m_parent.push_back(i);
-    }
-}
+int main() {
+    Graph graph{};
 
-void UnionFind::make_union(int32_t x_index, int32_t y_index) {
-    // 긴 트리가 있다면 긴 트리의 자식으로 작은 쪽의 트리를 달아두는 방식으로
-    // 구현하는 것이 높이를 줄일 수 있다고 소개되어 있지만, size에 대한 정보도 관리해야 하니 일단은 구분하지 않고 연결하자.
     //
-    // Notes:
-    // x \in A를 테스트 하는 방법으로 bloom filter도 있구나...
-    // 일단 가장 어려운 건 이런 걸 사용했을 때 효과를 볼 데이터 세트를 얻는 게 가장 힘들지 않을까.
-    // 일단 충분히 큰 데이터를 상대로 연산해야 "아 이거 정말로 좋구나"라고 느낄 수 있을텐데.
-    auto x_root = find_root(x_index);
+    //   1 - 2 - 3
+    //  /     \   \
+    // 0 ----- 4 -- 5 - 6 - 7
+    //  \          /
+    //   +--------+
+    //
+    std::vector<std::string> vertices {
+        "0", "1", "2", "3", "4", "5", "6", "7"
+    };
+    std::vector<Edge> edges {
+        { .source=0, .destination=1, .weight=1 },
+        { .source=0, .destination=4, .weight=2 },
+        { .source=0, .destination=5, .weight=10 },
+        { .source=1, .destination=2, .weight=2 },
+        { .source=2, .destination=3, .weight=4 },
+        { .source=2, .destination=4, .weight=2 },
+        { .source=3, .destination=5, .weight=3 },
+        { .source=4, .destination=5, .weight=6 },
+        { .source=5, .destination=6, .weight=3 },
+        { .source=6, .destination=7, .weight=4 },
+    };
 
-    m_parent[y_index] = x_root;
+    graph.insert_node(vertices.begin(), vertices.end());
+    graph.insert_edge(edges.begin(), edges.end());
+
+    //   1 - 2
+    //  /     \
+    // 0 ----- 4
+    //    ^^^^
+    // 예를 들어 이건 사이클을 만들기 때문에 스킵해야 함 (stable_sort가 아니라 이게 먼저 연결되면 문제 없긴 하지만...)
+    graph.print_kruskal_mst();
+
+    // 최소비용으로 신장한 모습
+    //   1 - 2 - 3
+    //  /         \
+    // 0 ----- 4    5 - 6 - 7
+
+    return 0;
 }
-
-int32_t UnionFind::find_root(int32_t index) {
-
-
-    int32_t id = index;
-    while (id != m_parent[id]) {
-        m_parent[id] = m_parent[m_parent[id]]; // path compression
-        id = m_parent[id];
-    }
-
-    return id;
-}
-
-bool UnionFind::is_in_same_set(int32_t x_index, int32_t y_index) {
-    auto x_root = find_root(x_index);
-    auto y_root = find_root(y_index);
-
-    return x_root == y_root;
-}
-
-int main() { return 0; }
