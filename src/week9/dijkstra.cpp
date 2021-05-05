e55a57349dd003111bb9638e417d81a71dc6eaca
peru0 <65155257+peru0@users.noreply.github.com>
2021-05-05 11:38:29 +0900

    dijkstra wip (metro)

---
+++ src/week9/dijkstra.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2/catch.hpp"
+#include "spdlog/spdlog.h"
+#include "dijkstra.h"
+
+TEST_CASE("Testing a map", "[dijkstra]") {
+
+}
+
+
