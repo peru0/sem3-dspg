673b832b6109eb7718af77ca8b57e5533289773d
peru0 <65155257+peru0@users.noreply.github.com>
2021-05-12 17:18:45 +0900

    Update unit test to match current implementation of binary heap

--- src/binary_heap_test.cpp
+++ src/binary_heap_test.cpp
@@ -3,19 +3,19 @@
 #include "binary_heap.hpp"
 
 TEST_CASE("Test binary heap", "[binary heap]" ) {
-    BinaryHeap<int> heap;
+    BinaryHeap<int, int> heap;
 
     SECTION("inserting elements in descending order") {
-        heap.insert(5);
-        heap.insert(4);
-        heap.insert(3);
-        heap.insert(2);
-        heap.insert(1);
+        heap.insert(5, 42);
+        heap.insert(4, 43);
+        heap.insert(3, 44);
+        heap.insert(2, 45);
+        heap.insert(1, 46);
 
-        REQUIRE(heap.pop() == 1);
-        REQUIRE(heap.pop() == 2);
-        REQUIRE(heap.pop() == 3);
-        REQUIRE(heap.pop() == 4);
-        REQUIRE(heap.pop() == 5);
+        REQUIRE(heap.pop() == 46);
+        REQUIRE(heap.pop() == 45);
+        REQUIRE(heap.pop() == 44);
+        REQUIRE(heap.pop() == 43);
+        REQUIRE(heap.pop() == 42);
     }
 }
